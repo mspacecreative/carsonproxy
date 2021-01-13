17d679735b22eb3a23ff04fa7b7bec8e7aa3c9bf
Matt <matt@mspacecreative.com>
2021-01-13 16:20:12 -0400

    c

--- footer.php
+++ footer.php
@@ -4,10 +4,10 @@
 				<div class="innerContainer">
 					<!-- copyright -->
 					<p class="desktopOnly">Carson Proxy Advisors Ltd.<br />
-					50 Carroll Street, Toronto, Ontario, M4M 3G38&nbsp;&nbsp;|&nbsp;&nbsp;1-888-511-1228&nbsp;&nbsp;|&nbsp;&nbsp;416-804-0825</p>
+					50 Carroll Street, Toronto, Ontario, M4M 3G38&nbsp;&nbsp;|&nbsp;&nbsp;<a href="tel:18885111228" style="color: #333; text-decoration: none;">1-888-511-1228</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="tel:14168040825" style="color: #333; text-decoration: none;">416-804-0825</a></p>
 					<p class="mobileOnly">Carson Proxy Advisors Ltd.<br />
 					50 Carroll Street, Toronto, Ontario, M4M 3G3<br />
-					<a href="tel:18885111228" style="color: #fff;">1-888-511-1228</a>&nbsp;&nbsp;|&nbsp;<a href="tel:14168040825" style="color: #fff;">416-804-0825</a></p>
+					<a href="tel:18885111228" style="color: #fff;">1-888-511-1228</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="tel:14168040825" style="color: #fff;">416-804-0825</a></p>
 					<p class="copyright">
 						&copy; <?php echo esc_html( date( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?>. <?php echo __('All rights reserved.'); ?></a>
 					</p>
